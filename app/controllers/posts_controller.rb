e954588e269d61987c2c121324999988b2f37475
gooch <goochlapeyronie@gmail.com>
2021-07-09 16:05:28 +0200

    f/input search ok

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,11 @@ class PostsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @posts = Post.all
+    if params[:query].present?
+      @posts = Post.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @posts = Post.all
+    end
   end
 
   def show
