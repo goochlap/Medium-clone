766a478bc2d6c3236f13df21c062fbfc61dd81c7
gooch <goochlapeyronie@gmail.com>
2021-07-09 18:27:05 +0200

    f/top liking many times done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,22 @@ class LikesController < ApplicationController
   before_action :find_post
 
   def create
-    @post.likes.create(user_id: current_user.id)
-    redirect_to post_path(@post)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
+
+    redirect_to request.referrer
   end
 
   private
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 end
