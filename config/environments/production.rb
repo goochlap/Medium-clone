4269da820f51edffe6b9bf242c04654a0601a493
gooch <goochlapeyronie@gmail.com>
2021-07-31 16:42:18 +0200

    fix/drop DB & rename all APP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Test_Room_In_Touch_production"
+  # config.active_job.queue_name_prefix = "Medium_Clone_production"
 
   config.action_mailer.perform_caching = false
 
