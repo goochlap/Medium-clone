e8bb259ca1261eb084327617a73e69284756e595
gooch <goochlapeyronie@gmail.com>
2021-08-02 15:32:31 +0200

    f/js function private|public btn ok

--- app/javascript/packs/action-buttons.js
+++ app/javascript/packs/action-buttons.js
@@ -1,12 +1,20 @@
 const actionButtons = () => {
 
-  // buttons from user stories show
+  // buttons public|private from user stories show
+  const buttonPublic  = document.querySelector('.public-btn');
   const buttonPrivate = document.querySelector('.private-btn');
+  const publicStatus  = document.querySelector('.post-status-public');
+  const privateStatus = document.querySelector('.post-status-private');
 
-  if (buttonPrivate) {
-    
+  if (buttonPublic && buttonPrivate) {
+
+      buttonPublic.addEventListener('click', () => {
+        privateStatus.style.display = 'none';
+        publicStatus.style.display  = 'block';
+      });
       buttonPrivate.addEventListener('click', () => {
-        console.log('hey');
+        publicStatus.style.display  = 'none';
+        privateStatus.style.display = 'block';
       });
     
   }
